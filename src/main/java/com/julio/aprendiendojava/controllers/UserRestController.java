9519a02c5c001b6758936b83e66bfd9937ed59c3
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-07 11:33:23 -0500

    add method to get details dto

--- src/main/java/com/julio/aprendiendojava/controllers/UserRestController.java
+++ src/main/java/com/julio/aprendiendojava/controllers/UserRestController.java
@@ -1,6 +1,7 @@
 package com.julio.aprendiendojava.controllers;
 
 import com.julio.aprendiendojava.models.User;
+import com.julio.aprendiendojava.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +10,27 @@ import java.util.Map;
 
 @RestController
 public class UserRestController {
-    @GetMapping("/get-details")
-    public Map<String, Object> details() {
+    @GetMapping("/get-details-map")
+    public Map<String, Object> detailsMap() {
         User user = new User("Julio Angel", "Ucharima Ortiz");
 
         Map<String, Object> response = new HashMap<>();
 
         response.put("title", "Hi world!!");
         response.put("user", user);
-        //response.put("lastname", user.getLastname());
+        response.put("lastname", user.getLastname());
 
         return response;
+    }
+
+    @GetMapping("/get-details-dto")
+    public UserDto detailsDto() {
+        User user = new User("Julio Angel", "Ucharima Ortiz");
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hi world!! I'm using Spring 3");
+
+        return userDto;
     }
 }
