46abe1a2cc7fadceaace294125b364d59ac42489
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-07 12:30:07 -0500

    add list method

--- src/main/java/com/julio/aprendiendojava/controllers/UserRestController.java
+++ src/main/java/com/julio/aprendiendojava/controllers/UserRestController.java
@@ -5,8 +5,7 @@ import com.julio.aprendiendojava.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class UserRestController {
@@ -32,5 +31,30 @@ public class UserRestController {
         userDto.setTitle("Hi world!! I'm using Spring 3");
 
         return userDto;
+    }
+
+    @GetMapping("/get-user-list")
+    public List<User> userList(){
+
+/*
+        La lógica de negocio no se maneja en el controlador. Sino en un
+        services o repository
+*/
+
+        User user = new User("Julio Angel", "Ucharima Ortiz");
+        User user2 = new User("Lionel Andres", "Messi Cuccitini");
+        User user3 = new User("Cristiano Ronaldo", "Dos Santos Aveiro");
+
+
+//      Usando la clase helpers Arrays
+        List<User> users = Arrays.asList(user, user2, user3);
+/*
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+*/
+
+        return users;
     }
 }
