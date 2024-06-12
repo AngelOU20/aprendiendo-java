27f51c4aa1a20c19f1831407a76acf2ecf5542ce
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-12 11:12:27 -0500

    add list method

--- src/main/java/com/julio/aprendiendojava/controllers/UserController.java
+++ src/main/java/com/julio/aprendiendojava/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.julio.aprendiendojava.controllers;
 import com.julio.aprendiendojava.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -13,11 +16,20 @@ public class UserController {
     @GetMapping("/details")
     public String details(Model model) {
         User user = new User("Julio Angel", "Ucharima Ortiz");
-
+        user.setEmail("julio@angel.com");
         model.addAttribute("title", "Hi world!!");
         model.addAttribute("user", user);
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Hi world!!");
+        return "list";
     }
 
     @GetMapping("/operations")
